e29a7fe94884db43ff7f9f0be094b7595028a642
Anne Vollmann <vollmann.anne@web.de>
2025-07-03 12:26:21 +0200

    firebase-notes displayed

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,78 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NoteListService {
 
-  constructor() { }
+export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+
+  // items$;
+  // items;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+
+    // this.items$ = collectionData(this.getNotesRef());
+    // this.items = this.items$.subscribe((list) => {
+    //   list.forEach(element => {
+    //     console.log(element)
+    //   });
+    // })
+  }
+
+  ngOnDestroy() {
+    this.unsubTrash();
+    this.unsubNotes();
+    // this.items.unsubscribe();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  // getSingleDocRef(colId: string, docId: string) {
+  //   return doc(collection(this.firestore, colId), docId);
+  // }
 }
